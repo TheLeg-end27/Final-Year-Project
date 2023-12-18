1dabd4aafe79738d2e2f202823bd892ed306c293
TheLeg-end27 <noechenjiang@gmail.com>
2023-12-18 14:54:36 +0000

    basic client side messaging
    Former-commit-id: ffead5589fed702222f3decd7a7e618e719c75ce

--- map/map/static/myapp/map.js
+++ map/map/static/myapp/map.js
@@ -1,4 +1,73 @@
+function openDatabase() {
+  return new Promise((resolve, reject) => {
+      const request = indexedDB.open('MapMessagesDB', 1);
 
+      request.onerror = function(event) {
+          reject("Database error: " + event.target.errorCode);
+      };
+
+      request.onsuccess = function(event) {
+          resolve(event.target.result);
+      };
+
+      request.onupgradeneeded = function(event) {
+          let db = event.target.result;
+          db.createObjectStore('messages', { autoIncrement: true });
+      };
+  });
+}
+
+function storeMessage(lat, lng, message,  db) {
+  let transaction = db.transaction(['messages'], 'readwrite');
+  let objectStore = transaction.objectStore('messages');
+  let request = objectStore.add({ lat, lng, message });
+
+  request.onerror = function(event) {
+      console.error("Error adding message to DB: ", event.target.error);
+  };
+
+  request.onsuccess = function(event) {
+      console.log("Message added to DB successfully");
+  };
+}
+
+function loadMessages(db) {
+  let transaction = db.transaction(['messages']);
+  let objectStore = transaction.objectStore('messages');
+  let request = objectStore.openCursor();
+
+  request.onerror = function(event) {
+      console.error("Error fetching messages from DB: ", event.target.error);
+  };
+
+  request.onsuccess = function(event) {
+      let cursor = event.target.result;
+      if (cursor) {
+          let marker = L.marker([cursor.value.lat, cursor.value.lng]).addTo(container);
+          marker.bindPopup(cursor.value.message);
+          cursor.continue();
+      }
+  };
+}
+
+function initMap(db) {
+  container.on('click', function(e) {
+    let message = prompt("Enter your message for this location:", "");
+    if (message) {
+        L.marker([e.latlng.lat, e.latlng.lng]).addTo(container)
+        .bindPopup(message);
+        storeMessage(e.latlng.lat, e.latlng.lng, message, db);
+    }
+  });
+}
+
+
+openDatabase().then(db => {
+  loadMessages(db);
+  initMap(db);
+}).catch(error => {
+  console.error("Failed to open DB: ", error);
+});
 //const container = L.map('map', {
 //  center: L.latLng(40.416775, -3.703790),
 //  zoom: 5,
